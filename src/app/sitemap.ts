4135b367f11f18f2d966568c42f75294c934c1fe
Your name here <elia.orsini@hotmail.com>
2024-08-08 22:50:22 +0100

    fix: generate sitemap manually

---
+++ src/app/sitemap.ts
@@ -0,0 +1,35 @@
+import Magazine from "@/types/IMagazine";
+import convertSlugToPath from "@/utils/convertSlugToPath";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const magazinesDataRaw = await fetch(process.env.URL + `/api/magazines`);
+  const magazinesData = await magazinesDataRaw.json();
+
+  const sitemapPages: any[] = [];
+
+  magazinesData.map((mag: Magazine) => {
+    if (mag.doesHaveAPage) {
+      const sitemapEntry = {
+        url: `https://immortal-mags.xyz/magazine/${convertSlugToPath(
+          mag.name
+        )}`,
+        lastModified: new Date(),
+        changeFrequency: "daily",
+        priority: 0.8,
+      };
+
+      sitemapPages.push(sitemapEntry);
+    }
+  });
+
+  return [
+    {
+      url: "https://immortal-mags.xyz",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    ...sitemapPages,
+  ];
+}
