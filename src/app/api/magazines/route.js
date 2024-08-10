eae88322cf96bb708c9882cd623bb859a601705a
Your name here <elia.orsini@hotmail.com>
2024-08-10 16:41:54 +0100

    fix: force revalidation

--- src/app/api/magazines/route.js
+++ src/app/api/magazines/route.js
@@ -1,6 +1,7 @@
 export async function GET() {
   const data = await fetch(
-    `${process.env.CLOUDFLARE_WORKER}/v1/table/${process.env.NOTION_DB}`
+    `${process.env.CLOUDFLARE_WORKER}/v1/table/${process.env.NOTION_DB}`,
+    { next: { revalidate: 10 } }
   ).then((res) => res.json());
 
   return Response.json(data);
