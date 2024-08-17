f9122d3db975d74138ec730fdd8112ff3930003a
Your name here <elia.orsini@hotmail.com>
2024-08-17 11:11:18 +0100

    feat: add Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer/Footer";
 import "./globals.css";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -11,7 +12,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer: React.FC = () => {
+  return (
+    <div className="text-xs m-2 sm:m-10">
+      <p>immortal mags © 2024</p>
+
+      <p>
+        created by{" "}
+        <a className="underline" target="_blank" href="https://elia-orsini.com">
+          elia
+        </a>{" "}
+        with {`<3`}
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
