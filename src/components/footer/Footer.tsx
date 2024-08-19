6a4875856aba878e924869dec283ba518fe7e53c
Your name here <elia.orsini@hotmail.com>
2024-08-19 13:32:53 +0100

    style: update footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,39 @@
 const Footer: React.FC = () => {
   return (
-    <div className="text-xs m-2 sm:m-10">
-      <p>immortal mags © 2024</p>
+    <div className="flex flex-col gap-y-6 sm:gap-y-6 justify-between mx-2 sm:mx-10 mb-10 text-xs">
+      <div className="flex flex-col gap-y-1 sm:gap-y-1">
+        <p>
+          immortal mags is aiming to be the biggest online repository of
+          indipendent magazines.
+        </p>
 
-      <p>
-        created by{" "}
-        <a className="underline" target="_blank" href="https://elia-orsini.com">
-          elia
-        </a>{" "}
-        with {`<3`}
-      </p>
+        <p>
+          send us an{" "}
+          <a
+            className="underline"
+            target="_blank"
+            href="mailto:elia.orsini@hotmail.com"
+          >
+            email
+          </a>{" "}
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-y-1 sm:gap-y-1">
+        <p>immortal mags © 2024</p>
+
+        <p>
+          created by{" "}
+          <a
+            className="underline"
+            target="_blank"
+            href="https://elia-orsini.com"
+          >
+            elia
+          </a>{" "}
+          with {`<3`}
+        </p>
+      </div>
     </div>
   );
 };
