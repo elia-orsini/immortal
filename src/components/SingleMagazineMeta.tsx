b6dae17eafa500cff294cb579020a308f5478883
Your name here <elia.orsini@hotmail.com>
2024-08-20 00:26:54 +0100

    feat: add bilingual meta tag

--- src/components/SingleMagazineMeta.tsx
+++ src/components/SingleMagazineMeta.tsx
@@ -4,20 +4,30 @@ import convertIssuerAYearToText from "@/utils/convertIssuerAYearToText";
 const SingleMagazineMeta: React.FC<{ meta: Magazine }> = ({ meta }) => {
   return (
     <div className="flex flex-col m-4 sm:gap-y-2">
-      <section className="w-full w-11/12 sm:w-8/12 sm:mx-auto flex flex-col sm:flex-row justify-between uppercase text-sm">
+      <div className="w-full w-11/12 sm:w-8/12 sm:mx-auto flex flex-col sm:flex-row justify-between uppercase text-sm">
         {meta.city !== "-" && <p className="">based in {meta.city}</p>}
 
         <p>{convertIssuerAYearToText(meta.issuesPerYear.toString())}</p>
 
         {meta.since && <p className="">since {meta.since}</p>}
-      </section>
+      </div>
 
-      {meta.link && (
-        <section className="w-full w-11/12 sm:w-8/12 sm:mx-auto flex flex-col sm:flex-row justify-between uppercase text-sm">
-          <a href={meta.link} target="_blank" className="underline">
-            website
-          </a>
-        </section>
+      {(meta.link || meta.language) && (
+        <div className="w-full w-11/12 sm:w-8/12 sm:mx-auto flex flex-col sm:flex-row justify-between uppercase text-sm">
+          {meta.link && (
+            <a href={meta.link} target="_blank" className="underline">
+              website
+            </a>
+          )}
+
+          {meta.language && (
+            <p>
+              bilingual {"("}
+              {meta.language.join(", ")}
+              {")"}
+            </p>
+          )}
+        </div>
       )}
     </div>
   );
