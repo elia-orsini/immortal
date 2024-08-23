2d0ce5999881dd0de1af9e01b30d08055196ae4e
Your name here <elia.orsini@hotmail.com>
2024-08-23 07:50:50 +0100

    feat: random magazine section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import AnimatedIntro from "@/components/AnimatedIntro";
 import MagazinesSection from "@/components/MagazinesSection/MagazinesSection";
 import Magazine from "@/types/IMagazine";
+import convertTitleToSlug from "@/utils/convertTitleToSlug";
+import Link from "next/link";
 
 const dataFetcher = async (): Promise<{
   filteredMags: Magazine[];
+  magazinesData: Magazine[];
 }> => {
   const magazinesData = await fetch(process.env.URL + `/api/magazines`, {
     next: { revalidate: parseInt(process.env.REVALIDATE_TIME!) },
@@ -13,19 +16,39 @@ const dataFetcher = async (): Promise<{
     (mag: Magazine) => !mag.field.includes("too-big")
   );
 
-  return { filteredMags };
+  return { filteredMags, magazinesData };
 };
 
 export default async function Home() {
-  const { filteredMags } = await dataFetcher();
+  const { filteredMags, magazinesData } = await dataFetcher();
+
+  const magWithPages = magazinesData.filter(
+    (mag: Magazine) => mag.doesHaveAPage
+  );
+  const randomIndex = Math.floor(Math.random() * magWithPages.length);
+  const randomMag = magWithPages[randomIndex];
 
   return (
-    <main className="flex min-h-screen w-screen flex-col mb-20">
-      <AnimatedIntro />
+    <>
+      <main className="flex min-h-screen w-screen flex-col mb-20">
+        <AnimatedIntro />
 
-      <section className="mx-2 sm:mx-auto">
-        <MagazinesSection filteredMags={filteredMags} />
+        <section className="mx-2 sm:mx-auto">
+          <MagazinesSection filteredMags={filteredMags} />
+        </section>
+      </main>
+
+      <section className="mb-6">
+        <p className="text-xs sm:mx-10">
+          undecided? bored? feeling bold? discover a{" "}
+          <Link
+            className="underline"
+            href={`/magazine/${convertTitleToSlug(randomMag.name)}`}
+          >
+            random magazine
+          </Link>
+        </p>
       </section>
-    </main>
+    </>
   );
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ const Footer: React.FC = () => {
     <div className="flex flex-col gap-y-6 sm:gap-y-6 justify-between mx-2 sm:mx-10 mb-10 text-xs">
       <div className="flex flex-col gap-y-1 sm:gap-y-1">
         <p>
-          immortal mags aims to be the largest online repository of
-          independent magazines.
+          immortal mags aims to be the largest online repository of independent
+          magazines.
         </p>
 
         <p>
